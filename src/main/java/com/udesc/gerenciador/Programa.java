a9c51b6e0ff0772e7724ed33f587dff252cc03c6
Vinicius Pereira <vinicius.pereira@edu.joinville.sc.gov.br>
2026-05-07 17:45:07 -0300

    Versão funcional

--- src/main/java/com/udesc/gerenciador/Programa.java
+++
@@ -1,9 +0,0 @@
-package com.udesc.gerenciador;
-import java.util.Scanner;
-
-public class Programa {
-        public static void main(String args[]){
-            Scanner input = new Scanner(System.in);
-            Programa app = new Programa();          
-        }
-}
